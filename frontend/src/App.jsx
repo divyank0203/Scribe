faa34af213825908354a6676e5322f5b677dc5ec
divyank0203 <divyankh444@gmail.com>
2026-06-29 12:52:47 +0530

    frontend vite + taliwindcss setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 classname='text-3xl'>Hello Vite+React+Tailwind</h1>
+    </>
+  )
+}
+
+export default App
